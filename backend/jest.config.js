f5a030dde815d60129880579caebea5710b348f6
Salvijus Grambovskis <deathphasers@gmail.com>
2026-04-25 01:18:27 +0300

    Initial project structure

---
+++ backend/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/server.ts'
+  ],
+  coverageDirectory: 'coverage',
+}
---
+++ frontend/jest.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/tests/**/*.test.(ts|tsx)'],
+  moduleNameMapper: {
+    '\\.(css|svg|png|jpg)$': '<rootDir>/tests/__mocks__/fileMock.ts',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        jsx: 'react-jsx',
+        esModuleInterop: true,
+        types: ['jest', 'node'],
+      },
+    },
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/main.tsx',
+  ],
+  coverageDirectory: 'coverage',
+}
