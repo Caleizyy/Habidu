0166b18df21e3d46dc5ede0005fb280e228765d3
Karolis Malinauskas <karolinauskas@gmail.com>
2026-05-06 12:48:09 +0300

    Remove: mongooose root dependency, user/habit models, seed script. Modify .env.example and server.ts to pull from .env file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,11 +6,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = 'mongodb://localhost:27017/habit_db';
+const MONGO_URI = process.env.MONGO_URI;
 
 app.get('/', (req, res) => {
   res.send('Backend is alive');
 });
+
+if (!MONGO_URI) throw new Error('MONGO_URI is not defined in the environment variables');
 
 mongoose
   .connect(MONGO_URI)
