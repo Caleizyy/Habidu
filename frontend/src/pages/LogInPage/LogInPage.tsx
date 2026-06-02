67c2317cbacdf8fcc3a780fa8f3bebe2d1efd99e
Karolis Malinauskas <karolinauskas@gmail.com>
2026-06-02 15:25:49 +0300

    Remove LogInPage and SignUpPage

--- frontend/src/pages/LogInPage/LogInPage.tsx
+++
@@ -1,9 +0,0 @@
-import { PageLayout } from '@/components/layout/PageLayout';
-
-export function LogInPage() {
-  return (
-    <PageLayout title="Login">
-      <div></div>
-    </PageLayout>
-  );
-}
--- frontend/src/pages/SignUpPage/SignUpPage.tsx
+++
@@ -1,9 +0,0 @@
-import { PageLayout } from '@/components/layout/PageLayout';
-
-export function SignUpPage() {
-  return (
-    <PageLayout title="Sign Up">
-      <div></div>
-    </PageLayout>
-  );
-}
