2586f8658e0ae6c1dc7725650496456ca3293bb6
Karolis Malinauskas <karolinauskas@gmail.com>
2026-06-02 15:13:15 +0300

    Tweak card stacking under very small screens (<400px width) and adjust Input min width

--- frontend/src/components/habits/HabitCard.tsx
+++ frontend/src/components/habits/HabitCard.tsx
@@ -10,7 +10,7 @@ export default function HabitsCard({ habits }: HabitCardProps) {
   return (
     <Card className="h-full w-full">
       <div className="flex flex-row items-center justify-between px-6">
-        <Input placeholder="Enter habit name" className="mt-2 h-[4vh] w-[25vw]" />
+        <Input placeholder="Enter habit name" className="mt-2 h-[4vh] w-[25vw] min-w-40" />
       </div>
       <div className="mt-4 flex min-h-0 flex-1 flex-col gap-4 overflow-y-auto px-6 py-1">
         {habits.map((habit) => (
