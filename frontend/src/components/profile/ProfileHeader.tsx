40e105d3a783964665ab10aa36c9c0a9626bbcc5
Salvijus Grambovskis <salvijus.grambovskis1@gmail.com>
2026-06-23 21:43:10 +0000

    Merged in bug/359-2-profile-headers (pull request #28)
    Bug/359 2 profile headers
    
    * Removed custom My Profile header from profile page
    
    * fixed mistaken color changes
    
    
    Approved-by: Jonas Stempickij
    Approved-by: Karolis Malinauskas

--- frontend/src/components/profile/ProfileHeader.tsx
+++ frontend/src/components/profile/ProfileHeader.tsx
@@ -23,7 +23,6 @@ export function ProfileHeader({ user, isEditing, onEditClick }: ProfileHeaderPro
   return (
     <>
       <div className="flex items-center justify-between">
-        <h1 className="text-4xl font-bold text-black">My Profile</h1>
         {!isEditing && (
           <Button variant="outline" onClick={onEditClick} className="h-[5vh] w-[7vw]">
             <Pencil className="mr-2 h-[1vh] w-[1vw]" />
