de4b44c89963f608c4ac6c3898f92e59d4124872
Salvijus Grambovskis <salvijus.grambovskis1@gmail.com>
2026-06-29 08:40:34 +0000

    Merged in feature/300-habit-filter (pull request #41)
    Feature/300 habit filter
    
    * Added habit filtering to habits in habit creation page
    
    * Merged with new dev and merge conflicts
    
    
    Approved-by: Adrian

--- frontend/src/components/habits/HabitCard.tsx
+++ frontend/src/components/habits/HabitCard.tsx
@@ -2,22 +2,30 @@ import { Card } from '@/components/ui/Card';
 import { Input } from '@/components/ui/Input';
 import { Habit } from '@/types/habit';
 import HabitItemCard from './HabitItemCard';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
+
 interface HabitCardProps {
   habits: Habit[];
   setRefreshKey: Dispatch<SetStateAction<number>>;
 }
 
 export default function HabitsCard({ habits }: Readonly<HabitCardProps>) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <Card className="h-full w-full">
       <div className="flex flex-row items-center justify-between px-6">
-        <Input placeholder="Enter habit name" className="mt-2 h-[4vh] w-[25vw] min-w-40" />
+        <Input placeholder="Enter habit name" className="mt-2 h-[4vh] w-[25vw] min-w-40" onChange={handleChange} />
       </div>
       <div className="mt-4 flex min-h-0 flex-1 flex-col gap-4 overflow-y-auto px-6 py-1">
-        {habits.map((habit) => (
-          <HabitItemCard key={habit._id} habit={habit} />
-        ))}
+        {habits
+          .filter((habit) => habit.name.toLowerCase().includes(searchTerm))
+          .map((habit) => (
+            <HabitItemCard key={habit._id} habit={habit} />
+          ))}
       </div>
     </Card>
   );
