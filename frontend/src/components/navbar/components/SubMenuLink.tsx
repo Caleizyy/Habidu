ea05c2f01ac3db4b2fa322f1c8a4c5c9a7d11c0b
Adrian <adrian.klimasevski@gmail.com>
2026-07-02 11:46:45 +0300

    bug: navabar is navigatible using only keyboard

--- frontend/src/components/navbar/components/SubMenuLink.tsx
+++ frontend/src/components/navbar/components/SubMenuLink.tsx
@@ -4,7 +4,7 @@ import { MenuItem } from '../types';
 const SubMenuLink = ({ item }: { item: MenuItem }) => {
   return (
     <Link
-      className="hover:bg-muted hover:text-accent-foreground flex min-w-80 flex-row gap-4 rounded-md p-3 leading-none no-underline transition-colors outline-none select-none"
+      className="hover:bg-muted hover:text-accent-foreground focus-visible:ring-ring flex min-w-80 flex-row gap-4 rounded-md p-3 leading-none no-underline transition-colors select-none focus-visible:ring-2"
       to={item.url}
     >
       <div className="text-foreground">{item.icon}</div>
